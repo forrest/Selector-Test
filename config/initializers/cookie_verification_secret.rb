b7d74352a98ecbb574aab5ec228adb9d347dbc83
Forrest Zeisler <forrest@smibs.com>
2010-06-01 17:45:21 -0600

    fighting with configuration for heroku

--- config/initializers/cookie_verification_secret.rb
+++ config/initializers/cookie_verification_secret.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '906c72f79c9d51d2b4b3cda6452264fd372a69aa775f590f33372b0f75c0f5199180156f499062ea31b8732d0f2ba396b72c120ff94ebb4575407a5b5b797e6d';
+# ActionController::Base.cookie_verifier_secret = '906c72f79c9d51d2b4b3cda6452264fd372a69aa775f590f33372b0f75c0f5199180156f499062ea31b8732d0f2ba396b72c120ff94ebb4575407a5b5b797e6d';
